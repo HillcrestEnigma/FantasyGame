108a0ede04fb278cadbfe8e708b9c27f10d0fb77
hotcoolplay <47504421+hotcoolplay@users.noreply.github.com>
2021-06-11 12:26:00 -0400

    Add files via upload

--- Location.java
+++ Location.java
@@ -1,12 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package homework;
-
 /**
- *
+ * This is an interface for locations in the games
+ * 
+ * @version 06-122-2021
  * @author JingKun
  */
 public interface Location {
