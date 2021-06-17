d7f8ae0d7e8b32b8518e69e0e460d7961d1097a1
Paul Lee <contact@paullee.dev>
2021-06-17 11:53:14 -0400

    add procedurally generated DarkRoom constructor

--- DarkRoom.java
+++ DarkRoom.java
@@ -1,22 +1,28 @@
 import java.util.ArrayList;
+import java.util.Random; // https://www.tutorialspoint.com/java/util/java_util_random.htm
 class DarkRoom extends Room {
     boolean radioactive;
 	
     /**
      * Constructs a DarkRoom
-     * @param name The name of the room.
      * @param x The x co-ordinate of the room
      * @param y The y co-ordinate of the room
      * @param goldAmt The amount of the gold.
      * @param radioactive If radioactivity is present
      */
-    public DarkRoom(String name, int x, int y, boolean radioactive) {
-        super(name, x, y);
+    public DarkRoom(int x, int y, int goldAmt, boolean radioactive) {
+        super("Castle", x, y);
         this.position = new Position (name, x, y);
 	this.radioactive = radioactive;
 	this.inventory = new Inventory();
-        inventory.addItem(new Item("Gold"));
-        inventory.setItemQuantity("Gold", goldAmt);
+        inventory.addItem(new Item("Gold", goldAmt));
+    }
+
+    public DarkRoom(int x, int y, long seed) {
+        Random rng = new Random(seed);
+        int goldAmt = rng.nextInt(5) + 5;
+        boolean radioactive = rng.nextInt(100) < 10;
+        this(x, y, goldAmt, radioactive);
     }
 	
     /**
--- Location.java
+++ Location.java
@@ -17,6 +17,7 @@ public class Location {
                 return room;
             }
         }
+        return null;
     }
 
     /**
