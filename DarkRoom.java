2e96194f3e67274ae41ba34126d0f7f72d3a9c14
hotcoolplay <47504421+hotcoolplay@users.noreply.github.com>
2021-06-17 11:18:13 -0400

    Made DarkRoom a subclass of Room

--- DarkRoom.java
+++ DarkRoom.java
@@ -1,63 +1,51 @@
-
-
 import java.util.ArrayList;
-
-/**
- * DarkRooms have certain properties that Elves can interact with.
- *
- *
- */
-class DarkRoom {
-	String name;
-	int gold;
-	boolean radioactive;
+class DarkRoom extends Room {
+    int goldAmt;
+    boolean radioactive;
+    Position pos;
+    Inventory inventory;
+    ArrayList<Elf> elves;
 	
-	ArrayList<Elf> elves;
+    /**
+     * Constructs a DarkRoom
+     * @param name The name of the room.
+     * @param x The x co-ordinate of the room
+     * @param y The y co-ordinate of the room
+     * @param goldAmt The amount of the gold.
+     * @param radioactive If radioactivity is present
+     */
+    public DarkRoom(String name, int x, int y, int goldAmt, boolean radioactive) {
+        super(name, x, y);
+        this.pos = new Position (name, x, y);
+	this.radioactive = radioactive;
+	this.inventory = new Inventory();
+        inventory.addItem(new Item("Gold"));
+        inventory.setItemQuantity("Gold", goldAmt);
+	elves = new ArrayList<Elf>();
+    }
 	
-	/**
-	 * Constructs a DarkRoom
-	 * @param name The name of the room.
-	 * @param gold The amount of the gold.
-	 * @param radioactive If radioactivity is present
-	 */
-	DarkRoom(String name, int gold, boolean radioactive)
-	{
-		this.name = name;
-		this.gold = gold;
-		this.radioactive = radioactive;
-		
-		elves = new ArrayList<Elf>();
-	}
-	
-	/**
-	 * Have Elf interact with DarkRoom.
-	 * @param elf
-	 */
-	void enter(Elf elf)
-	{
-		elves.add(elf);
-		
-		if (radioactive)
-			elf.exposeToRadiation();
-		
-		gold = elf.takeGold(gold);
-	}
-	
-	/**
-	 * Remove Elf from room.
-	 * @param elf
-	 */
-	void exit(Elf elf)
-	{
-		elves.remove(elf);
-	}
-
-	/**
-	 * A description of this DarkRoom
-	 * @return
-	 */
-	String getName() {
-		
-		return name;
-	}
+    /**
+     * Have Elf interact with DarkRoom.
+     *
+     * @param elf
+     */
+    public boolean enter(Elf elf) {
+        if(elf.getPos().getLocation().equals("Nowhere") 
+                && !pos.getLocation().equals("Entrance")) {
+            elves.add(elf);
+            return false;
+        }
+        else if (elf.getPos().getLocation().equals("Nowhere") 
+                && pos.getLocation().equals("Entrance")) {
+            elf.getPos().setLocation("Entrance");
+            return true;
+        }
+        else if (Math.abs(elf.getPos().getRoomX() - pos.getRoomX()) < 2
+                && Math.abs(elf.getPos().getRoomY() - pos.getRoomY()) < 2) {
+            elf.getPos().setRoomX(pos.getRoomX());
+            elf.getPos().setRoomY(pos.getRoomY());
+            return true;
+        }
+        else return false;
+    }
 }
