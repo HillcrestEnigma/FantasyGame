f849b74c8e9e8464fdfb3666680dbac5437f3fe9
hotcoolplay <hotcoolplay@gmail.com>
2021-06-18 09:39:00 -0400

    Bug fixes

--- Location.java
+++ Location.java
@@ -1,3 +1,5 @@
+import java.util.List;
+import java.util.ArrayList;
 /**
  * This is an interface for locations in the games
  * 
--- Room.java
+++ Room.java
@@ -8,7 +8,6 @@ public class Room {
     public Inventory inventory;
     public Position position;
     public Room(String location, int x, int y) {
-        this.name = name;
         this.position = new Position(location, x, y);
         this.inventory = new Inventory();
     }
@@ -21,19 +20,20 @@ public class Room {
      */
     public boolean enter(Elf elf) {
         if(elf.getPos().getLocation().equals("Nowhere") 
-                && !name.equals("Entrance")) {
+                && !position.getLocation().equals("Entrance")) {
             return false;
         }
         else if (elf.getPos().getLocation().equals("Nowhere") 
-                && name.equals("Entrance")) {
+                && position.getLocation().equals("Entrance")) {
             elf.getPos().setLocation("Entrance");
             return true;
         }
-        else if (Math.abs(elf.getPos().getRoomX() - x) < 2
-                && Math.abs(elf.getPos().getRoomY() - y) < 2) {
+        else if (Math.abs(elf.getPos().getRoomX() - position.getRoomX()) < 2
+                && Math.abs(elf.getPos().getRoomY() - position.getRoomY()) < 2) 
+        {
             elf.getPos().setLocation("Room");
-            elf.getPos().setRoomX(x);
-            elf.getPos().setRoomY(y);
+            elf.getPos().setRoomX(position.getRoomX());
+            elf.getPos().setRoomY(position.getRoomY());
             return true;
         }
         else return false;
