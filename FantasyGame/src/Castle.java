bd7dd08b9209ef98f2017af70d9a22c0d25eb03b
hotcoolplay <hotcoolplay@gmail.com>
2021-06-18 23:44:06 -0400

    Changes castle size to 25 rooms and rooms have 10-20 gold

--- FantasyGame/src/Castle.java
+++ FantasyGame/src/Castle.java
@@ -14,12 +14,12 @@ class Castle extends Location {
      */
     public Castle(long seed) {
         Random rng = new Random(seed);
-        int goldAmt = rng.nextInt(5) + 5;
+        int goldAmt = rng.nextInt(10) + 10;
         int x = 0;
         int y = 0;
         int direction;
         addRoom(new DarkRoom(0, 0, rng.nextLong()));
-        while (getSize() < 100) {
+        while (getSize() < 25) {
             direction = rng.nextInt(4);
             if (direction == 0) x++;
             else if (direction == 1) x--;
